8b9331582d39744782866010c81697c322722ec9
Asmee-tech <asmee.khimani@gmail.com>
2026-09-05 10:59:40 +0200

    Add files via upload

---
+++ First.py
@@ -0,0 +1,17 @@
+import cv2
+img=cv2.imread("opencv-assets-main/pika.png",0)
+img[0][200]=0
+print(img[0][200])
+cv2.imshow("screen1",img)
+cv2.waitKey(0)
+cv2.imwrite("Black_and_White_Pika.png",img)
+
+# img=cv2.imread("opencv-assets-main/opencv.png")
+# b,g,r= cv2.split (img)
+# cv2.imshow("screen2",b)
+# cv2.waitKey(5000)
+# cv2.imshow("screen3",g)
+# cv2.waitKey(5000)
+# cv2.imshow("screen4",r)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
