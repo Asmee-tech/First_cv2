8657a8b01e9bc03d5de9ab6bc6c63a4e300cd0cf
Asmee-tech <asmee.khimani@gmail.com>
2026-09-12 10:06:32 +0200

    Comment out image processing in First.py
    Comment out image processing code for testing.

--- First.py
+++ First.py
@@ -1,10 +1,10 @@
 import cv2
-img=cv2.imread("opencv-assets-main/pika.png",0)
-img[0][200]=0
-print(img[0][200])
-cv2.imshow("screen1",img)
-cv2.waitKey(0)
-cv2.imwrite("Black_and_White_Pika.png",img)
+# img=cv2.imread("opencv-assets-main/pika.png",0)
+# img[0][200]=0
+# print(img[0][200])
+# cv2.imshow("screen1",img)
+# cv2.waitKey(0)
+# cv2.imwrite("Black_and_White_Pika.png",img)
 
 # img=cv2.imread("opencv-assets-main/opencv.png")
 # b,g,r= cv2.split (img)
@@ -15,3 +15,15 @@ cv2.imwrite("Black_and_White_Pika.png",img)
 # cv2.imshow("screen4",r)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
+# img=cv2.imread("opencv-assets-main/flower.jpg",)
+# img[0][200]=200
+# print(img[0][200])
+# cv2.imshow("Original Image",img)
+# cv2.waitKey(0)
+
+# img=cv2.imread("opencv-assets-main/car.jpg",)
+# img[0][200]=200
+# print(img[0][200])
+# cv2.imshow("Car Image",img)
+# cv2.waitKey(0)
